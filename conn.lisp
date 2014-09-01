d7793c01b34c228c3fef9894168dc5b58a5e8d3c
Aaron France <aaron.l.france@gmail.com>
2014-09-02 00:02:06 +0200

    Store the connection options in the connection objet

--- conn.lisp
+++ conn.lisp
@@ -3,12 +3,13 @@
 
 (defclass connection ()
   ;; TODO maybe this should hold an instance of a parser
-  ((conn :accessor conn :initarg :conn)))
+  ((conn :accessor conn :initarg :conn)
+   (conn-options :accessor conn-options :initarg :options)))
 
 (defun make-connection (&optional (host "localhost") (port 9042)); TODO: &key version compression)
   (let ((conn (usocket:socket-stream
                (usocket:socket-connect host port :element-type '(unsigned-byte 8)))))
     (options conn)
     (force-output conn)
-    (parse-packet (read-single-packet conn))
-    (make-instance 'connection :conn conn)))
+    (let ((options (parse-packet (read-single-packet conn))))
+      (make-instance 'connection :conn conn :options options))))
