4b7949abe226ae3121907b886ffaec6c60613758
Aaron France <aaron.l.france@gmail.com>
2014-09-01 23:56:02 +0200

    Add TODO for passing in version info and compression type

--- conn.lisp
+++ conn.lisp
@@ -5,7 +5,7 @@
   ;; TODO maybe this should hold an instance of a parser
   ((conn :accessor conn :initarg :conn)))
 
-(defun make-connection (&optional (host "localhost") (port 9042))
+(defun make-connection (&optional (host "localhost") (port 9042)); TODO: &key version compression)
   (let ((conn (usocket:socket-stream
                (usocket:socket-connect host port :element-type '(unsigned-byte 8)))))
     (options conn)
