bb4633c46af976bbce876e5efd3f9ee7d24e59fb
Aaron France <aaron.l.france@gmail.com>
2014-09-01 21:05:02 +0200

    Round out create-connection to send the OPTIONS header

--- conn.lisp
+++ conn.lisp
@@ -2,6 +2,13 @@
 
 
 (defclass connection ()
-  ((streams :accessor streams)
-   (version :accessor vsn)))
+  ;; TODO maybe this should hold an instance of a parser
+  ((conn :accessor conn :initarg :conn)))
 
+(defun make-connection (&optional (host "localhost") (port 9042))
+  (let ((conn (usocket:socket-stream
+               (usocket:socket-connect host port :element-type '(unsigned-byte 8)))))
+    (options conn)
+    (force-output conn)
+    (parse-packet (read-single-packet conn))
+    (make-instance 'connection :conn conn)))
