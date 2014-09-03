6fa869f09b57aeffc122ca1f72ef8532472b7641
Aaron France <aaron.l.france@gmail.com>
2014-09-03 16:31:52 +0200

    Add coverage output to tests

--- run-tests.lisp
+++ run-tests.lisp
@@ -1,4 +1,17 @@
 (in-package #:cl-user)
 
+#+sbcl
+(require :sb-cover)
+
+#+sbcl
+(progn
+  (declaim (optimize sb-cover:store-coverage-data))
+  (asdf:oos 'asdf:load-op :cqlcl :force t))
+
 (ql:quickload :cqlcl)
 (asdf:test-system :cqlcl)
+
+#+sbcl
+(progn
+  (sb-cover:report "./coverage/")
+  (declaim (optimize (sb-cover:store-coverage-data 0))))
