1b7e158d26704d89686050c96ccfbf4996cf35a9
Aaron France <aaron.l.france@gmail.com>
2014-09-24 14:28:20 +0200

    Unimplement #'ip=

--- types.lisp
+++ types.lisp
@@ -97,7 +97,10 @@
        1)))
 
 (defun ip= (ip1 ip2)
-  (equalp (addr ip1) (addr ip2)))
+  ;; (equalp (addr ip1) (addr ip2))
+  ;; TODO: NOT IMPLEMENTED
+  t
+  )
 
 (defmethod ip-to-integer ((ip ipv6))
   ;; TODO: properly implement this
